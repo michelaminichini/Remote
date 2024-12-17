3e5ff6c18f58823ed821ca586b8141c9688d8fc9
marti <martina.proietti2@studio.unibo.it>
2024-12-17 09:37:56 +0100

    calendar improvement

--- Template.Web/Features/Home/HomeViewModel.cs
+++ Template.Web/Features/Home/HomeViewModel.cs
@@ -16,26 +16,32 @@ namespace Template.Web.Features.Home
             CurrentYear = monthStart.Year;
 
             var current = monthStart;
+
+            // Allineamento con il primo giorno della settimana (Lunedì)
+            var startOfWeek = current.AddDays(-(int)current.DayOfWeek + (int)DayOfWeek.Monday);
             var week = new List<DayViewModel>();
 
             while (current.Month == monthStart.Month || week.Any())
             {
-                if (current.Month != monthStart.Month && current.DayOfWeek == DayOfWeek.Monday)
+                week.Add(new DayViewModel
                 {
-                    break;
-                }
+                    Date = startOfWeek,
+                    Events = new List<string>()
+                });
 
-                week.Add(new DayViewModel { Date = current, Events = new List<string>() });
+                startOfWeek = startOfWeek.AddDays(1);
 
-                if (current.DayOfWeek == DayOfWeek.Sunday)
+                if (startOfWeek.DayOfWeek == DayOfWeek.Monday)
                 {
                     Weeks.Add(week);
                     week = new List<DayViewModel>();
                 }
 
-                current = current.AddDays(1);
+                if (startOfWeek > current.AddMonths(1).AddDays(-1))
+                    break;
             }
         }
+
     }
 
     public class DayViewModel
