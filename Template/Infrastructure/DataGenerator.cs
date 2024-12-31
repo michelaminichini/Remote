102055269f121e16c743288e1a45a10293999ccb
Michela <michela.minichini2@studio.unibo.it>
2024-12-31 14:57:21 +0100

    fixed small error

--- Template/Infrastructure/DataGenerator.cs
+++ Template/Infrastructure/DataGenerator.cs
@@ -93,7 +93,7 @@ namespace Template.Infrastructure
                 new User
                 {
                     Id = Guid.NewGuid(),
-                    Email = "ceo@aziendale.it",
+                    Email = "ceo@ceo.it",
                     Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", // SHA-256 of text "Test"
                     FirstName = "Paolo",
                     LastName = "Neri",
